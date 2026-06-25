fb9f1b144eecfeeeb1220e0ca1691fe3a406a761
lucas tavares <ltv2234@gmail.com>
2026-06-25 14:28:28 -0300

    Ajustes

--- TodoList.Domain/Entities/User.cs
+++ TodoList.Domain/Entities/User.cs
@@ -4,18 +4,21 @@ public class User
 {
     public Guid Id { get; private set; }
 
-    public string FirstName { get; private set; } = string.Empty;
+    public string FirstName { get; private set; }
 
-    public string LastName { get; private set; } = string.Empty;
+    public string LastName { get; private set; }
 
-    public string Email { get; private set; } = string.Empty;
+    public string Email { get; private set; }
 
-    protected User() { }
+    public string PasswordHash { get; private set; } = string.Empty;
+
+    protected User(){}
 
     public User(
         string firstName,
         string lastName,
-        string email)
+        string email,
+        string passwordHash)
     {
         if (string.IsNullOrWhiteSpace(firstName))
             throw new ArgumentException("O primeiro nome não pode estar vazio.", nameof(firstName));
@@ -23,11 +26,24 @@ public class User
             throw new ArgumentException("O sobrenome não pode estar vazio.", nameof(lastName));
         if (string.IsNullOrWhiteSpace(email))
             throw new ArgumentException("O email não pode estar vazio.", nameof(email));
+        if (string.IsNullOrWhiteSpace(passwordHash))
+            throw new ArgumentException(
+                "O hash da senha não pode estar vazio.", nameof(passwordHash));
 
         Id = Guid.NewGuid();
         FirstName = firstName;
         LastName = lastName;
         Email = email;
+        PasswordHash = passwordHash;
+    }
+
+    public void ChangePassword(string passwordHash)
+    {
+        if (string.IsNullOrWhiteSpace(passwordHash))
+            throw new ArgumentException(
+                "O hash da senha não pode estar vazio.", nameof(passwordHash));
+
+        PasswordHash = passwordHash;
     }
 
     public string FullName =>
