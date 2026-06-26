095302ff9d1a55d4e562e6e73fe764549cccc1f3
lucas tavares <ltv2234@gmail.com>
2026-06-26 11:26:51 -0300

    Feat: Melhorado estrutura do task

--- TodoList.Domain/Entities/TaskItem.cs
+++ TodoList.Domain/Entities/TaskItem.cs
@@ -1,27 +1,18 @@
-﻿using TodoList.Domain.Enums;
+﻿using TodoList.Domain.Common;
+using TodoList.Domain.Enums;
+using TodoList.Domain.Validations;
 
 namespace TodoList.Domain.Entities;
 
-public class TaskItem
+public class TaskItem : BaseEntity
 {
-    public Guid Id { get; private set; }
-
     public string Title { get; private set; } = string.Empty;
-
     public string Description { get; private set; } = string.Empty;
-
     public Enums.TaskStatus Status { get; private set; }
-
     public TaskPriority Priority { get; private set; }
-
     public Guid AssignedUserId { get; private set; }
-
     public Guid ProjectId { get; private set; }
-
     public Guid? SprintId { get; private set; }
-
-    public DateTime CreatedAt { get; private set; }
-
     public DateTime? CompletedAt { get; private set; }
 
     protected TaskItem() { }
@@ -29,30 +20,56 @@ public class TaskItem
     public TaskItem(
         string title,
         string description,
-        TaskPriority priority)
+        TaskPriority priority,
+        Guid projectId,
+        Guid? sprintId)
     {
+
+        TaskValidation.Validate(
+            title, description, projectId);
+
         Id = Guid.NewGuid();
         Title = title;
         Description = description;
+        ProjectId = projectId;
+        SprintId = sprintId;
         Priority = priority;
-
         Status = Enums.TaskStatus.Backlog;
         CreatedAt = DateTime.UtcNow;
     }
 
     public void AssignTo(Guid userId)
     {
+        if (userId == Guid.Empty)
+            throw new ArgumentException(
+                "Usuário inválido.");
+
         AssignedUserId = userId;
+        UpdatedAt = DateTime.UtcNow;
     }
-
-    public void MoveToDoing()
+    public void ChangeStatus(Enums.TaskStatus status)
     {
-        Status = Enums.TaskStatus.Doing;
+        Status = status;
+
+        UpdatedAt = DateTime.UtcNow;
+
+        if (status == Enums.TaskStatus.Done)
+            CompletedAt = DateTime.UtcNow;
+        else
+            CompletedAt = null;
     }
-
-    public void Complete()
+    public void Rename(
+    string title,
+    string description)
     {
-        Status = Enums.TaskStatus.Done;
-        CompletedAt = DateTime.UtcNow;
+        TaskValidation.Validate(
+            title,
+            description,
+            ProjectId);
+
+        Title = title;
+        Description = description;
+
+        UpdatedAt = DateTime.UtcNow;
     }
 }
---
+++ TodoList.Domain/Validate/TaskValidation.cs
@@ -0,0 +1,29 @@
+﻿using TodoList.Domain.Entities;
+using TodoList.Domain.Enums;
+
+namespace TodoList.Domain.Validations;
+
+public static class TaskValidation
+{ 
+    public static void Validate(
+        string title,
+        string description,
+        Guid project)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException(
+                "O título não pode estar vazio.",
+                nameof(title));
+
+        if (string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException(
+                "A descrição não pode estar vazia.",
+                nameof(description));
+
+        if(project == Guid.Empty)
+            throw new ArgumentException(
+                "O projeto não pode estar vazio.",
+                nameof(project));
+    } 
+
+}
