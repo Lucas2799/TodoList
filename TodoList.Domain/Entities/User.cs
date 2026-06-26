a222145448118225db1b7de79ffc56c0ec2f96c3
lucas tavares <ltv2234@gmail.com>
2026-06-26 11:26:38 -0300

    Feat: Melhorado estrutura do user

--- TodoList.Domain/Entities/User.cs
+++ TodoList.Domain/Entities/User.cs
@@ -1,15 +1,13 @@
-﻿namespace TodoList.Domain.Entities;
+﻿using TodoList.Domain.Common;
+using TodoList.Domain.Validations;
 
-public class User
+namespace TodoList.Domain.Entities;
+
+public class User : BaseEntity
 {
-    public Guid Id { get; private set; }
-
     public string FirstName { get; private set; }
-
     public string LastName { get; private set; }
-
     public string Email { get; private set; }
-
     public string PasswordHash { get; private set; } = string.Empty;
 
     protected User(){}
@@ -20,30 +18,40 @@ public class User
         string email,
         string passwordHash)
     {
-        if (string.IsNullOrWhiteSpace(firstName))
-            throw new ArgumentException("O primeiro nome não pode estar vazio.", nameof(firstName));
-        if (string.IsNullOrWhiteSpace(lastName))
-            throw new ArgumentException("O sobrenome não pode estar vazio.", nameof(lastName));
-        if (string.IsNullOrWhiteSpace(email))
-            throw new ArgumentException("O email não pode estar vazio.", nameof(email));
-        if (string.IsNullOrWhiteSpace(passwordHash))
-            throw new ArgumentException(
-                "O hash da senha não pode estar vazio.", nameof(passwordHash));
+        UserValidation.Validate(
+            firstName, lastName, email);
+
+        UserValidation.ValidatePasswordHash(passwordHash);
 
         Id = Guid.NewGuid();
         FirstName = firstName;
         LastName = lastName;
         Email = email;
         PasswordHash = passwordHash;
+        CreatedAt = DateTime.UtcNow; ;
     }
 
     public void ChangePassword(string passwordHash)
     {
-        if (string.IsNullOrWhiteSpace(passwordHash))
-            throw new ArgumentException(
-                "O hash da senha não pode estar vazio.", nameof(passwordHash));
+        UserValidation.ValidatePasswordHash(passwordHash);
 
         PasswordHash = passwordHash;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void UpdateProfile(
+        string firstName,
+        string lastName,
+        string email)
+    {
+        UserValidation.Validate(
+           firstName, lastName, email);
+
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+
+        UpdatedAt = DateTime.UtcNow;
     }
 
     public string FullName =>
---
+++ TodoList.Domain/Validate/UserValidation.cs
@@ -0,0 +1,38 @@
+﻿namespace TodoList.Domain.Validations;
+
+public static class UserValidation
+{
+    public static void Validate(
+        string firstName,
+        string lastName,
+        string email)
+    {
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new ArgumentException(
+                "O primeiro nome não pode estar vazio.",
+                nameof(firstName));
+
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new ArgumentException(
+                "O sobrenome não pode estar vazio.",
+                nameof(lastName));
+
+        if (string.IsNullOrWhiteSpace(email))
+            throw new ArgumentException(
+                "O e-mail não pode estar vazio.",
+                nameof(email));
+
+        if (!email.Contains('@'))
+            throw new ArgumentException(
+                "E-mail inválido.",
+                nameof(email));
+    }
+
+    public static void ValidatePasswordHash(string passwordHash)
+    {
+        if (string.IsNullOrWhiteSpace(passwordHash))
+            throw new ArgumentException(
+                "O hash da senha não pode estar vazio.",
+                nameof(passwordHash));
+    }
+}
