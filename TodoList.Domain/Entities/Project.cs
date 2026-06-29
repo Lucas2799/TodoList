74f0c0c3b6d2f2b39b147c8e9cccd695e6f9c3fa
lucas tavares <ltv2234@gmail.com>
2026-06-29 16:02:54 -0300

    Feat: Entities e validation de project

--- TodoList.Domain/Entities/Project.cs
+++ TodoList.Domain/Entities/Project.cs
@@ -1,27 +1,72 @@
-﻿namespace TodoList.Domain.Entities;
+﻿using TodoList.Domain.Common;
+using TodoList.Domain.Validations;
 
-public class Project
+namespace TodoList.Domain.Entities;
+
+public class Project : BaseEntity
 {
-    public Guid Id { get; private set; }
     public string Name { get; private set; } = string.Empty;
     public string Description { get; private set; } = string.Empty;
-
     public Guid WorkspaceId { get; private set; }
-
+    public bool IsDeleted { get; private set; }
     private readonly List<TaskItem> _tasks = [];
     public IReadOnlyCollection<TaskItem> Tasks => _tasks;
+    private readonly List<Sprint> _sprints = [];
+    public IReadOnlyCollection<Sprint> Sprints => _sprints.AsReadOnly();
 
     protected Project() { }
 
-    public Project(string name, string description)
+    public Project(string name, string description, Guid workspaceId)
     {
+        ProjectValidation.Validate(
+            name, description, workspaceId);
+
         Id = Guid.NewGuid();
         Name = name;
         Description = description;
+        WorkspaceId = workspaceId;
     }
 
     public void AddTask(TaskItem task)
     {
         _tasks.Add(task);
+        UpdatedAt = DateTime.UtcNow;
     }
+    public void RemoveTask(TaskItem task)
+    {
+        _tasks.Remove(task);
+        UpdatedAt = DateTime.UtcNow;
+    }
+    public void MoveToWorkspace(Guid workspaceId)
+    {
+        if (workspaceId == Guid.Empty)
+            throw new ArgumentException(
+                "Workspace inválido.",
+                nameof(workspaceId));
+
+        WorkspaceId = workspaceId;
+        UpdatedAt = DateTime.UtcNow;
+    }
+    public void ChangeName(string name)
+    {
+        Name = name;
+        UpdatedAt = DateTime.UtcNow;
+    }
+    public void DeleteProject() 
+    { 
+        if(_tasks.Any())
+            throw new InvalidOperationException(
+                "Não é possível deletar um projeto com tarefas associadas.");
+        IsDeleted = true;
+        UpdatedAt = DateTime.UtcNow;
+    }
+    public void AddSprint(Sprint sprint)
+    {
+         if (sprint == null)
+            throw new ArgumentNullException(nameof(sprint), "Sprint não pode ser nulo.");
+
+        _sprints.Add(sprint);
+        UpdatedAt = DateTime.UtcNow;
+    }
+
 }
---
+++ TodoList.Domain/Validate/ProjectValidation.cs
@@ -0,0 +1,29 @@
+﻿using TodoList.Domain.Entities;
+using TodoList.Domain.Enums;
+
+namespace TodoList.Domain.Validations;
+
+public static class ProjectValidation
+{
+    public static void Validate(
+        string name,
+        string description,
+        Guid workspaceId)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException(
+                "O título não pode estar vazio.",
+                nameof(name));
+
+        if (string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException(
+                "A descrição não pode estar vazia.",
+                nameof(description));
+
+        if (workspaceId == Guid.Empty)
+            throw new ArgumentException(
+                "O workspace não pode estar vazio.",
+                nameof(workspaceId));
+    }
+
+}
